e0fe235a27efe486ce07f42cd3da2071653038e7
matejgerek <maftvej@gmail.com>
2023-03-03 13:28:14 +0100

    Add vigenere encrypting and decrypting

--- assignment_1/zadanie1.c
+++ assignment_1/zadanie1.c
@@ -3,9 +3,49 @@
 #include <string.h>
 
 
-// main accepting command line arguments
-int main(int argc, char *argv[])
-{
+#define ASCII_SIZE 128
+#define ERROR_MESSAGE "chyba\n"
+#define NULL_TERMINATOR '\0'
+
+int update_key_index(int key_index, int key_len) {
+    key_index++;
+    if (key_index >= key_len) {
+        key_index = 0;
+    }
+    return key_index;
+}
+
+void vigenere_encrypt(char *plaintext, char *key, char *ciphertext) {
+    int plaintext_len = strlen(plaintext);
+    int key_len = strlen(key);
+    int key_index = 0;
+    for (int i = 0; i < plaintext_len; i++) {
+        int encrypted_character = (int) plaintext[i] + (int) key[key_index];
+        if (encrypted_character >= ASCII_SIZE) {
+            encrypted_character = encrypted_character - ASCII_SIZE;
+        }
+        ciphertext[i] = (char) encrypted_character;
+        key_index = update_key_index(key_index, key_len);
+    }
+    ciphertext[plaintext_len] = NULL_TERMINATOR;
+}
+
+void vigenere_decrypt(char *ciphertext, char *key, char *plaintext) {
+    int ciphertext_length = strlen(ciphertext);
+    int key_length = strlen(key);
+    int key_index = 0;
+    for (int i = 0; i < ciphertext_length; i++) {
+        int decrypted_char = (int) ciphertext[i] - (int) key[key_index];
+        if (decrypted_char < 0) {
+            decrypted_char = decrypted_char + ASCII_SIZE;
+        }
+        plaintext[i] = (char) decrypted_char;
+        key_index = (key_index + 1) % key_length;
+    }
+    plaintext[ciphertext_length] = NULL_TERMINATOR;
+}
+
+int main(int argc, char *argv[]) {
     int s_flag = 0;
     int d_flag = 0;
 
@@ -13,63 +53,76 @@ int main(int argc, char *argv[])
     char *input_file_value = NULL;
     char *output_file_value = NULL;
 
-    // parse command line arguments
-    for (int i = 1; i < argc; i++)
-    {
+    for (int i = 1; i < argc; i++) {
         char *arg = argv[i];
-
-        if (strcmp(arg, "-s") == 0) {
-            s_flag = 1;
-            continue;
+        switch (arg[0]) {
+            case '-':
+                switch (arg[1]) {
+                    case 's':
+                        s_flag = 1;
+                        break;
+                    case 'd':
+                        d_flag = 1;
+                        break;
+                    case 'p':
+                        if (i + 1 < argc) {
+                            password_value = argv[i + 1];
+                            i++;
+                        } else {
+                            printf(ERROR_MESSAGE);
+                            return 1;
+                        }
+                        break;
+                    case 'i':
+                        if (i + 1 < argc) {
+                            input_file_value = argv[i + 1];
+                            i++;
+                        } else {
+                            printf(ERROR_MESSAGE);
+                            return 1;
+                        }
+                        break;
+                    case 'o':
+                        if (i + 1 < argc) {
+                            output_file_value = argv[i + 1];
+                            i++;
+                        } else {
+                            printf(ERROR_MESSAGE);
+                            return 1;
+                        }
+                        break;
+                    default:
+                        printf(ERROR_MESSAGE);
+                        break;
+                }
+                break;
+            default:
+                printf(ERROR_MESSAGE);
+                break;
         }
-        if (strcmp(arg, "-d") == 0) {
-            d_flag = 1;
-            continue;
-        }
-
-        if (strcmp(arg, "-p") == 0) {
-            if (i + 1 < argc) {
-                password_value = argv[i + 1];
-                i++;
-            } else {
-                printf("Error: -p flag requires a value.\n");
-                return 1;
-            }
-            continue;
-        }
-
-        if (strcmp(arg, "-i") == 0) {
-            if (i + 1 < argc) {
-                input_file_value = argv[i + 1];
-                i++;
-            } else {
-                printf("Error: -i flag requires a value.\n");
-                return 1;
-            }
-            continue;
-        }
-
-        if (strcmp(arg, "-o") == 0) {
-            if (i + 1 < argc) {
-                output_file_value = argv[i + 1];
-                i++;
-            } else {
-                printf("Error: -o flag requires a value.\n");
-                return 1;
-            }
-            continue;
-        }
-        printf("Error: unknown flag: %s", arg);
     }
 
     if (s_flag == 1 && d_flag == 1) {
-        printf("Error: -s and -d flags cannot be used together.\n");
+        printf(ERROR_MESSAGE);
         return 1;
     }
+
     if (s_flag == 0 && d_flag == 0) {
-        printf("Error: -s or -d flag must be used.\n");
+        printf(ERROR_MESSAGE);
         return 1;
     }
+
+    char plaintext[100] = "Hello&$}, world!";
+    char key[100] = "secret";
+    char ciphertext[100];
+    char decrypted_plaintext[100];
+
+    vigenere_encrypt(plaintext, key, ciphertext);
+    printf("Encrypted message: %s\n", ciphertext);
+
+    vigenere_decrypt(ciphertext, key, decrypted_plaintext);
+    printf("Decrypted message: %s\n", decrypted_plaintext);
+
 
     return 0;
 }
