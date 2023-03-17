7e5c6f96ef9039ec20823ea32d53e8a47bd97def
matejgerek <maftvej@gmail.com>
2023-03-17 15:51:15 +0100

    Move hash function

--- assignment_2/zadanie2.c
+++ assignment_2/zadanie2.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -35,27 +34,7 @@ void get_data_from_input(char *username, char *password, char *key);
 
 bool write_users(User users[], int num_users);
 
-/**
- * Computes the hash value of a given string.
- *
- * This function uses the djb2 hash algorithm to compute the hash value of
- * the given string. The hash value is an unsigned long integer that can be
- * used to uniquely identify the input string.
- *
- * @param str The string to hash.
- *
- * @return The hash value of the input string.
- */
-unsigned long hash(char *str) {
-    unsigned long hash = 5381;
-    int c;
-
-    while ((c = *str++))
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
-    return hash;
-}
-
+unsigned long hash(char *str);
 
 int main(void) {
     User users[MAX_USERS];
@@ -238,5 +217,26 @@ void print_users(User users[], int num_users) {
     for (int i = 0; i < num_users; i++) {
         print_user(users[i]);
     }
+}
+
+/**
+ * Computes the hash value of a given string.
+ *
+ * This function uses the djb2 hash algorithm to compute the hash value of
+ * the given string. The hash value is an unsigned long integer that can be
+ * used to uniquely identify the input string.
+ *
+ * @param str The string to hash.
+ *
+ * @return The hash value of the input string.
+ */
+unsigned long hash(char *str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
 }
 
